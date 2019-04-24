54afb957e405602da56b99d23f3f668b9772d8e0
milosz-survicate <milosz.watroba@gmail.com>
2019-04-24 10:35:24 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "pagerank"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.1" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.1" % "provided"
---
+++ src/main/scala/PageRank.scala
@@ -0,0 +1,48 @@
+import org.apache.spark.sql.SparkSession
+
+object PageRank {
+  def main(args: Array[String]) {
+    val inputPath = args(0)
+    val iterations = args(1).toInt
+    val printTopNumber = args(2).toInt
+
+    val spark = SparkSession
+      .builder
+      .appName("PageRank")
+      .getOrCreate()
+
+    val lines = spark.read.textFile(inputPath).rdd
+
+    val links = lines
+      .filter(!_.startsWith("#"))
+      .filter(_.split("\\s").length == 2)
+      .map(line => line.split("\\s") match {
+        case Array(fromNodeId, toNodeId) => (fromNodeId, toNodeId)
+      })
+      .distinct()
+      .groupByKey()
+      .cache()
+
+    var ranks = links.mapValues(_ => 1.0)
+
+    for (_ <- 1 to iterations) {
+      val contributions = links
+        .join(ranks)
+        .flatMap { case (_, (neighbors, rank)) =>
+          neighbors.map(neighbor => (neighbor, rank / neighbors.size))
+        }
+
+      ranks = contributions
+        .reduceByKey(_ + _)
+        .mapValues(0.15 + 0.85 * _)
+    }
+
+    ranks
+      .collect()
+      .sortBy(_._2)(Ordering[Double].reverse)
+      .take(printTopNumber)
+      .foreach(t => println(s"Page: ${t._1}, Rank: ${t._2}"))
+
+    spark.stop()
+  }
+}
